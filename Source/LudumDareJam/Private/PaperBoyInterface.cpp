1c9b0ace22af91bdb99eeedb73e5c28bfc8cd66a
YunYun <Janminecraft1234@gmail.com>
2023-05-01 16:54:22 -0400

    Interface and more Sounds

---
+++ Source/LudumDareJam/Private/PaperBoyInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PaperBoyInterface.h"
+
+// Add default functionality here for any IPaperBoyInterface functions that are not pure virtual.
